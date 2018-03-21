5e2d760d39aaeee19cd1c9b891f8cde80e1cd828
Wes <wes_dillingham@harvard.edu>
2018-03-21 12:28:29 -0400

    this kinda works, but doesnt handle spaces in column1

--- package_stats.py
+++ package_stats.py
@@ -37,3 +37,13 @@ with gzip.open('/tmp/contents.gz', 'rb') as f_in:
 
 #neither the gunzip or the urlretreive complain if the file already exists, so just going to be okay with it overwriting existing files.
 
+#begin parsing test file:
+f = open("/tmp/contents.txt", "r")
+g = open("/tmp/contents_column2.txt", "w")
+
+for line in f:
+    if line.strip():
+        g.write("\t".join(line.split()[1:]) + "\n")
+
+f.close()
+g.close()
